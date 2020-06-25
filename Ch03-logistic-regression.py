7300695756aeb1d3bc3aa3550611800bcd6afff1
jingjielim <jingjie3000@gmail.com>
2020-06-25 21:08:23 +0800

    Add manual and sklearn LogReg

--- Ch03-logistic-regression.py
+++ Ch03-logistic-regression.py
@@ -28,3 +28,24 @@ plt.yticks([0.0,0.5,1.0])
 ax = plt.gca()
 ax.yaxis.grid(True)
 plt.show()
+
+# Create a plot that illustrates cost of classifying single-sample instance for different values of phi(z)
+# Main point is that we penalize wrong predictions with an increasingly larger cost 
+# %% codecell
+
+def cost_1(z):
+    return -np.log(sigmoid(z))
+def cost_0(z):
+    return -np.log(1-sigmoid(z))
+z = np.arange(-10, 10, 0.1)
+phi_z = sigmoid(z)
+c1 = [cost_1(x) for x in z]
+plt.plot(phi_z, c1, label='J(w) if y=1')
+c0 = [cost_0(x) for x in z]
+plt.plot(phi_z, c0, linestyle='--', label='J(w) if y=0')
+plt.ylim(0.0,5.1)
+plt.xlim([0,1])
+plt.xlabel('$\phi$(z)')
+plt.ylabel('J(w)')
+plt.legend(loc='best')
+plt.show()
